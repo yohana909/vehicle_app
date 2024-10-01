8a941b23a23cbf93dab9196a3c5798e1a81f9a83
yohana909 <yohananagy@icloud.com>
2024-09-30 22:36:59 -0700

    codes

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -1,9 +1,67 @@
 // Importing Vehicle and Wheel classes
 import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
+// Assuming the Vehicle class is defined in another file
+import Vehicle from "./Vehicle.js";
+import Wheel from "./Wheel.js";
+
+class Motorbike extends Vehicle {
+  // Declare properties of the Motorbike class
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+
+  // Create a constructor that accepts the properties of the Motorbike class
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[]
+  ) {
+    // Call the constructor of the parent class, Vehicle
+    super(vin, color, make, model, year, weight, topSpeed, wheels);
+
+    // Initialize the properties of the Motorbike class
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+
+    // Check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+    this.wheels = wheels.length === 2 ? wheels : [new Wheel(), new Wheel()];
+  }
+
+  // Implement the wheelie method
+  wheelie() {
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+  }
+
+  // Override the printDetails method from the Vehicle class
+  printDetails() {
+    // Call the printDetails method of the parent class
+    super.printDetails();
+    
+    // Log the details of the Motorbike
+    console.log(`Details: VIN: ${this.vin}, Make: ${this.make}, Model: ${this.model}, Year: ${this.year}, Weight: ${this.weight}, Top Speed: ${this.topSpeed}, Color: ${this.color}, Wheels: ${this.wheels.length}`);
+  }
+}
+
+// Export the Motorbike class as the default export
+export default Motorbike;
 
 // TODO: The Motorbike class should extend the Vehicle class
-class Motorbike {
   // TODO: Declare properties of the Motorbike class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
@@ -23,4 +81,4 @@ class Motorbike {
 }
 
 // Export the Motorbike class as the default export
-export default Motorbike;
+
