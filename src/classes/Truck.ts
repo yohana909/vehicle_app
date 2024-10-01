8ef371ad7a743ad66c91d26dcf3d23a9bb82800f
yohana909 <yohananagy@icloud.com>
2024-09-30 22:59:18 -0700

    more code

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -1,12 +1,8 @@
 // import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
-import Vehicle from './Vehicle.js';
-import Motorbike from './Motorbike.js';
-import Car from './Car.js';
-import Wheel from './Wheel.js';
-import AbleToTow from '../interfaces/AbleToTow.js';
+
 
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck {
+
   // TODO: Declare properties of the Truck class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
@@ -17,17 +13,84 @@ class Truck {
     // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
 
   // TODO: Implement the tow method from the AbleToTow interface
-  tow(vehicle: Truck | Motorbike | Car): void {
+  
     // TODO: Get the make an model of the vehicle if it exists
     // TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
     // TODO: If it is, log that the vehicle is being towed
     // TODO: If it is not, log that the vehicle is too heavy to be towed
-  }
+  
 
   // TODO: Override the printDetails method from the Vehicle class
     // TODO: The method should call the printDetails method of the parent class
     // TODO: The method should log the details of the Truck
     // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+
+
+// Export the Truck class as the default export
+export default Truck;
+// Import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+import Vehicle from './Vehicle.js';
+import Motorbike from './Motorbike.js';
+import Car from './Car.js';
+import Wheel from './Wheel.js';
+import AbleToTow from '../interfaces/AbleToTow.js';
+
+// The Truck class should extend the Vehicle class and implement the AbleToTow interface
+class Truck extends Vehicle implements AbleToTow {
+  // Declare properties of the Truck class
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCapacity: number;
+
+  // Create a constructor that accepts the properties of the Truck class
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ) {
+    // Call the constructor of the parent class, Vehicle
+    super(vin, color, make, model, year, weight, topSpeed, wheels);
+    
+    // Initialize the properties of the Truck class
+    this.towingCapacity = towingCapacity;
+
+    // Check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+    this.wheels = wheels.length === 4 ? wheels : [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+  }
+
+  // Implement the tow method from the AbleToTow interface
+  tow(vehicle: Truck | Motorbike | Car): void {
+    // Get the make and model of the vehicle if it exists
+    const { make, model, weight } = vehicle;
+
+    // Check if the vehicle's weight is less than or equal to the truck's towing capacity
+    if (weight <= this.towingCapacity) {
+      console.log(`${make} ${model} is being towed by the truck.`);
+    } else {
+      console.log(`${make} ${model} is too heavy to be towed by this truck.`);
+    }
+  }
+
+  // Override the printDetails method from the Vehicle class
+  printDetails() {
+    // Call the printDetails method of the parent class
+    super.printDetails();
+    
+    // Log the details of the Truck
+    console.log(`Details: VIN: ${this.vin}, Make: ${this.make}, Model: ${this.model}, Year: ${this.year}, Weight: ${this.weight}, Top Speed: ${this.topSpeed}, Color: ${this.color}, Towing Capacity: ${this.towingCapacity}, Wheels: ${this.wheels.length}`);
+  }
 }
 
 // Export the Truck class as the default export
